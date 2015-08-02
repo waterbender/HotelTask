1452d00139d93bdfe21ed6515b19bc5099b5c0db
waterbender <pask.eugene@gmail.com>
2015-08-02 12:09:38 +0300

    Initial Commit

---
+++ HotelTasks2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HotelTasks2
+//
+//  Created by  ZHEKA on 02.08.15.
+//  Copyright (c) 2015 Pasko Eugene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HotelTasks2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HotelTasks2
+//
+//  Created by  ZHEKA on 02.08.15.
+//  Copyright (c) 2015 Pasko Eugene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
